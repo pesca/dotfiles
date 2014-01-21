def346e1521dd81d3a1b671cdd07704d51091811
Philip Pesca <phil@pesca.co>
2014-01-20 21:58:02 -0800

    Add option to backup dotfiles and prettify output

--- script/install.sh
+++ script/install.sh
@@ -1,20 +1,62 @@
-DOTFILES_ROOT="`pwd`"
+#!/bin/bash
 
-link_files () {
-	ln -s $1 $2
-	printf "linked `basename \"$1\"` to ~/`basename \"$2\"`\n"
-} 
-
-for source in `find $DOTFILES_ROOT -maxdepth 2 -name \*.symlink`
-do
-	dest="$HOME/.`basename \"${source%.*}\"`"
-	echo $dest
-
-	if [ -f $dest ] || [ -d $dest ]
+error () {
+	if [ -n "$1" ]
 	then
-		rm -rf $dest
-		printf "removed $dest\n"
+		printf "error: $1"
+		printf "\n"
+	fi
+	printf "usage: script/install.sh [directory]\n"
+	exit
+}
+
+install () {
+	DF_ROOT="`pwd`"
+	DF_SOURCE="`find $DF_ROOT -maxdepth 2 -name \*.symlink`"
+
+	if [ -n "$1" ]
+	then
+		printf "\033[1;32mcreate\033[00m $1\n"
+		mkdir $1
 	fi
 
-	link_files $source $dest
-done
+	for source in $DF_SOURCE
+	do
+		dest="$HOME/.`basename \"${source%.*}\"`"
+		if [ -n "$1" ]
+		then
+			copy="`basename \"$dest\"`"
+			cp -r $dest $1/${copy#.}.symlink
+		fi
+
+		printf "\033[1;31mremoved\033[00m $dest\n"
+		rm -rf $dest
+	done
+
+	for source in $DF_SOURCE
+	do
+		dest="$HOME/.`basename \"${source%.*}\"`"
+		printf "\033[1;34mlinked\033[00m `basename \"$source\"`\n"
+		ln -s $source $dest
+	done
+}
+
+if [ $# -gt 1 ]
+then
+	error "Wrong argument count."
+fi
+
+# Overwrite dotfiles if a backup directory is not specified
+if [ -z "$1" ]
+then
+	install
+else
+	# Throw an error if the backup directory is a file or it already exists
+	if [ -f $1 ] || [ -d $1 ]
+	then
+		error "Directory already exists!"
+	else
+		install $1
+	fi
+fi
+
