41aaed60f35cb6af1fa4dbf4d77f1eefdf78b70c
Philip Pesca <phil.pesca@gmail.com>
2016-11-20 11:33:51 -0800

    Add backup feature
    The install script now backups existing configuration files.

--- script/install.py
+++ script/install.py
@@ -11,25 +11,27 @@ def install():
     base_dir = os.path.dirname(base_dir)
     home_dir = os.path.expanduser('~')
     sources = glob.glob(base_dir + "/*/*.symlink")
+
+    i = 0
+    while os.path.isdir(home_dir + "/dotfiles_backup_" + str(i)):
+        i = i + 1
+    backup_dir = home_dir + "/dotfiles_backup_" + str(i)
+    os.mkdir(backup_dir)
     
     for source in sources:
         source = os.path.splitext(source)[0]
         source = os.path.basename(source)
         dest = home_dir + "/." + source
 
-        if os.path.isfile(dest) or os.path.islink(dest):
-            os.remove(dest)
-        elif os.path.isdir(dest):
-            shutil.rmtree(dest)
-
-        print "removed {0}".format(dest)
+        if os.path.isfile(dest) or os.path.isdir(dest) or os.path.islink(dest):
+            os.rename(dest, backup_dir + "/" + source)
+            print "moved {0} to {1}".format(dest, backup_dir)
 
     for source in sources:
         dest = home_dir + "/." + os.path.basename(os.path.splitext(source)[0])
         os.symlink(source, dest)
 
         print "linked {0}".format(source)
-
 
 install()
 
